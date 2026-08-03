410941a7396839732659374f963c33d1610c79c7
Akuroii <2darkness465@gmail.com>
2026-08-03 09:19:11 +0300

    Delete gtm-v2 directory

--- gtm-v2/components/hero/Tagline.tsx
+++
@@ -1,9 +0,0 @@
-export function Tagline() {
-  return (
-    <p className="mt-4 font-[var(--font-display)] text-3xl italic leading-snug text-[var(--text)] sm:text-4xl md:text-5xl">
-      Every orbit brings us <span className="text-[var(--gold)]">closer</span>
-      <br />
-      to the Moon.
-    </p>
-  );
-}
--- gtm-v2/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Container } from "./Container";
-export { GlassPanel } from "./GlassPanel";
-export { Pill } from "./Pill";
-export { SectionLabel } from "./SectionLabel";
-export { SectionHeading } from "./SectionHeading";
-export { AnimatedNumber } from "./AnimatedNumber";
